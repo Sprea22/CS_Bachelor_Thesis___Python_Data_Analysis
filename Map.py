0239c185672c257c6466aecfedb1d50be04a909f
asp005 <asp005@post.uit.no>
2017-05-21 16:00:37 +0200

    Input section implemented

--- Map.py
+++ Map.py
@@ -1,149 +1,103 @@
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from matplotlib.offsetbox import AnchoredText
-import cartopy.io.shapereader as shpreader
-import shapefile
+import os
+import sys
 import matplotlib
-import matplotlib.patches as mpatches
+import pandas as pd
+import cartopy.crs as ccrs
 import matplotlib.cm as cmx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import cartopy.io.shapereader as shpreader
+
+def add_geom(axes, shapeInput, labelInput, colorInput):
+	axes.add_geometries(shapeInput, ccrs.Robinson(), edgecolor='black',label = labelInput, facecolor=colorInput, alpha=0.8)
+	return mpatches.Rectangle((0, 0), 1, 1, facecolor=colorInput)
 
 def main():
 	choiche = input("0 --> Display Norwegian counties map \n1 --> Display current input\n\n")
- # Downloaded from http://biogeo.ucdavis.edu/data/gadm2.8/shp/NOR_adm_shp.zip
+ 	# Downloaded from http://biogeo.ucdavis.edu/data/gadm2.8/shp/NOR_adm_shp.zip
 	fname = 'NOR/NOR_adm1.shp'
-	adm1_shapes = list(shpreader.Reader(fname).geometries())
+	NOR_shapes = list(shpreader.Reader(fname).geometries())
 	ax = plt.axes(projection=ccrs.Robinson())
+	ax.coastlines(resolution='10m')
+	ax.set_extent([4, 32, 57, 72], ccrs.Robinson())	
+
 	# Rendering the sea
 	#ax.stock_img()
-	ax.coastlines(resolution='10m')
-	# adm1_shapes[0] -> Ostfold
-	# adm1_shapes[1] -> Akershus
-	# adm1_shapes[2] -> Aust Agder
-	# adm1_shapes[3] -> Buskerud
-	# adm1_shapes[4] -> Finnmark
-	# adm1_shapes[5] -> Hedmark
-	# adm1_shapes[6] -> Hordaland
-	# adm1_shapes[7] -> More og Romsdal
-	# adm1_shapes[8] -> Nord Trondelag
-	# adm1_shapes[9] -> Nordland
-	# adm1_shapes[10] -> Oppland
-	# adm1_shapes[11] -> Oslo ?
-	# adm1_shapes[12] -> Rogaland
-	# adm1_shapes[13] -> Sor Trondelag
-	# adm1_shapes[14] -> Sogn og Fjordane
-	# adm1_shapes[15] -> Telemark 
-	# adm1_shapes[16] -> Troms
-	# adm1_shapes[17] -> Vest Agder
-	# adm1_shapes[18] -> Vestfold
+
+	# NOR_shapes[0] -> Ostfold
+	# NOR_shapes[1] -> Akershus
+	# NOR_shapes[2] -> Aust Agder
+	# NOR_shapes[3] -> Buskerud
+	# NOR_shapes[4] -> Finnmark
+	# NOR_shapes[5] -> Hedmark
+	# NOR_shapes[6] -> Hordaland
+	# NOR_shapes[7] -> More og Romsdal
+	# NOR_shapes[8] -> Nord Trondelag
+	# NOR_shapes[9] -> Nordland
+	# NOR_shapes[10] -> Oppland
+	# NOR_shapes[11] -> Oslo ?
+	# NOR_shapes[12] -> Rogaland
+	# NOR_shapes[13] -> Sor Trondelag
+	# NOR_shapes[14] -> Sogn og Fjordane
+	# NOR_shapes[15] -> Telemark 
+	# NOR_shapes[16] -> Troms
+	# NOR_shapes[17] -> Vest Agder
+	# NOR_shapes[18] -> Vestfold
 
 	if(choiche==0):
-		ax.add_geometries(adm1_shapes, ccrs.Robinson(),
-		                  edgecolor='black',label = "Norway", facecolor='gray', alpha=0.8)
+		norway = add_geom(ax, NOR_shapes, "Norway", "gray")
 
-		ax.add_geometries(adm1_shapes[4], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Finnmark", facecolor='red', alpha=0.8)
+		finnmark = add_geom(ax, NOR_shapes[4], "Finnmark", "red")
+		troms = add_geom(ax, NOR_shapes[16], "Troms", "springgreen")
+		nordland = add_geom(ax, NOR_shapes[9], "Nordland", "purple")
+		nord_trondelag = add_geom(ax, NOR_shapes[8], "Nord Trondelag", "orange")
+		sor_trondelag = add_geom(ax, NOR_shapes[13], "Sor Trondelag", "brown")
+		more_og_romsdal = add_geom(ax, NOR_shapes[7], "More og Romsdal", "yellow")
+		sogn_og_fjordane = add_geom(ax, NOR_shapes[14], "Sogn og Fjordane", "aqua")
+		hordaland = add_geom(ax, NOR_shapes[6], "Hordaland", "blue")
+		rogaland_og_agder = add_geom(ax, NOR_shapes[2], "Rogaland og Agder", "darkgreen")
+		rogaland_og_agder = add_geom(ax, NOR_shapes[12], "Rogaland og Agder", "darkgreen")
+		rogaland_og_agder = add_geom(ax, NOR_shapes[17], "Rogaland og Agder", "darkgreen")
 
-		ax.add_geometries(adm1_shapes[6], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Hordaland", facecolor='blue', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[7], ccrs.Robinson(),
-		                  edgecolor='black',  label = "More og Romsdal", facecolor='yellow', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[8], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Nord Trondelag", facecolor='orange', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[9], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Nordland", facecolor='purple', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[13], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Sor Trondelag", facecolor='brown', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[14], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Sogn og Fjordane", facecolor='aqua', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[16], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Troms", facecolor='springgreen', alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[2], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor='darkgreen', alpha=0.8)
-		ax.add_geometries(adm1_shapes[12], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor='darkgreen', alpha=0.8)
-		ax.add_geometries(adm1_shapes[17], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor='darkgreen', alpha=0.8)
-		norway = mpatches.Rectangle((0, 0), 1, 1, facecolor="gray")
-		finnmark = mpatches.Rectangle((0, 0), 1, 1, facecolor="red")
-		hordaland = mpatches.Rectangle((0, 0), 1, 1, facecolor="blue")
-		more_og_romsdal = mpatches.Rectangle((0, 0), 1, 1, facecolor="yellow")
-		nord_trondelag = mpatches.Rectangle((0, 0), 1, 1, facecolor="orange")
-		nordland = mpatches.Rectangle((0, 0), 1, 1, facecolor="purple")
-		sor_trondelag = mpatches.Rectangle((0, 0), 1, 1, facecolor="brown")			
-		sogn_og_fjordane = mpatches.Rectangle((0, 0), 1, 1, facecolor="aqua")
-		troms = mpatches.Rectangle((0, 0), 1, 1, facecolor="springgreen")
-		rogaland_og_agder = mpatches.Rectangle((0, 0), 1, 1, facecolor="darkgreen")
 		plt.title('Norway - Counties involved in Aquaculture business', fontsize=35)
 
 	
 	else:
+		# Decide the range of colors of the colorsMap
 		colorsMap='bwr'
-		cs = [6, 6.5 , 6.7 , 8 , 5 , 6.2, 5.8, 5.5, 6.9]
-		cm = plt.get_cmap(colorsMap)
-		minCs = min(cs)
-		cNorm = matplotlib.colors.Normalize(vmin=minCs, vmax=max(cs))
-		scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)	
-		colors = scalarMap.to_rgba(cs)	
+		dataInput = sys.argv[1]
+		inputSeries = pd.read_csv("countiesAverages.csv")
+		# Data input for each region
+		inputValues = [inputSeries[dataInput][0], inputSeries[dataInput][1], inputSeries[dataInput][2], inputSeries[dataInput][3],
+				inputSeries[dataInput][4], inputSeries[dataInput][5], inputSeries[dataInput][6], inputSeries[dataInput][7], inputSeries[dataInput][8]]
 
-		scalarMap.set_array(cs)
+		cm = plt.get_cmap(colorsMap)
+		mininputValues = min(inputValues)
+		cNorm = matplotlib.colors.Normalize(vmin=mininputValues, vmax=max(inputValues))
+		scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)	
+		colors = scalarMap.to_rgba(inputValues)	
+
+		# Colorbar displayed like a legend for the colors
+		scalarMap.set_array(inputValues)
 		plt.colorbar(scalarMap,label='Input Value')
 
-		ax.add_geometries(adm1_shapes, ccrs.Robinson(),
-		                  edgecolor='black',label = "Norway", facecolor='gray', alpha=0.8)
+		norway = add_geom(ax, NOR_shapes, "Norway", "gray")
 
-		ax.add_geometries(adm1_shapes[4], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Finnmark", facecolor=colors[0], alpha=0.8)
+		finnmark = add_geom(ax, NOR_shapes[4], "Finnmark", colors[0])
+		troms = add_geom(ax, NOR_shapes[16], "Troms", colors[1])
+		nordland = add_geom(ax, NOR_shapes[9], "Nordland", colors[2])
+		nord_trondelag = add_geom(ax, NOR_shapes[8], "Nord Trondelag", colors[3])
+		sor_trondelag = add_geom(ax, NOR_shapes[13], "Sor Trondelag", colors[4])
+		more_og_romsdal = add_geom(ax, NOR_shapes[7], "More og Romsdal", colors[5])
+		sogn_og_fjordane = add_geom(ax, NOR_shapes[14], "Sogn og Fjordane", colors[6])
+		hordaland = add_geom(ax, NOR_shapes[6], "Hordaland", colors[7])
+		rogaland_og_agder = add_geom(ax, NOR_shapes[2], "Rogaland og Agder", colors[8])
+		rogaland_og_agder = add_geom(ax, NOR_shapes[12], "Rogaland og Agder", colors[8])
+		rogaland_og_agder = add_geom(ax, NOR_shapes[17], "Rogaland og Agder", colors[8])
 
-		ax.add_geometries(adm1_shapes[6], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Hordaland", facecolor=colors[1], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[7], ccrs.Robinson(),
-		                  edgecolor='black',  label = "More og Romsdal", facecolor=colors[2], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[8], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Nord Trondelag", facecolor=colors[3], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[9], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Nordland", facecolor=colors[4], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[13], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Sor Trondelag", facecolor=colors[5], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[14], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Sogn og Fjordane", facecolor=colors[6], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[16], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Troms", facecolor=colors[7], alpha=0.8)
-
-		ax.add_geometries(adm1_shapes[2], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor=colors[8], alpha=0.8)
-		ax.add_geometries(adm1_shapes[12], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor=colors[8], alpha=0.8)
-		ax.add_geometries(adm1_shapes[17], ccrs.Robinson(),
-		                  edgecolor='black',  label = "Rogaland og Agder", facecolor=colors[8], alpha=0.8)
-		norway = mpatches.Rectangle((0, 0), 1, 1, facecolor="gray")
-		finnmark = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[0])
-		hordaland = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[1])
-		more_og_romsdal = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[2])
-		nord_trondelag = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[3])
-		nordland = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[4])
-		sor_trondelag = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[5])			
-		sogn_og_fjordane = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[6])
-		troms = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[7])
-		rogaland_og_agder = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[8])
-		plt.title('Norway - Current Input ', fontsize=35)
+		plt.title('Norway - '+sys.argv[1] , fontsize=35)
 
 
-
-	ax.set_extent([4, 32, 57, 72], ccrs.Robinson())	
 	labels = [
 		'Finnmark', 
 		'Troms', 	
